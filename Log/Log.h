8e4962a665efa53b3d2448491e523870b5532b91
Ales M. Michalek <Ales.M.Michalek@gmail.com>
2016-01-31 11:03:14 +0100

    Initial Commit

---
+++ Log/Log.h
@@ -0,0 +1,19 @@
+//
+//  Log.h
+//  Log
+//
+//  Created by Aleš M. Michálek on 31.01.16.
+//  Copyright © 2016 Deuterium-Ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Log.
+FOUNDATION_EXPORT double LogVersionNumber;
+
+//! Project version string for Log.
+FOUNDATION_EXPORT const unsigned char LogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Log/PublicHeader.h>
+
+
